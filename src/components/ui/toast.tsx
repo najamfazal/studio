e3838630f55e31cbee52fa7e1256df7ae4458ec4
Najam Fazal <fazal.najam@gmail.com>
2025-09-17 11:39:12 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -17,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-4 left-1/2 -translate-x-1/2 z-[100] flex max-h-screen w-full flex-col gap-2 p-4 md:max-w-sm",
+      "fixed bottom-4 left-1/2 -translate-x-1/2 z-[100] flex max-h-screen w-full flex-col gap-2 p-4 sm:max-w-sm",
       className
     )}
     {...props}
